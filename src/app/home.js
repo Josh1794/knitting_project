0b5e6bc044d433cb84c6e33eb4580730f6ff178b
Joshua Grossman <46975990+Josh1794@users.noreply.github.com>
2024-07-15 15:37:33 -0400

    Adding Supabase backend and auth

---
+++ src/app/home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>signed in</p>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
 import styles from './page.module.css';
 import SignInPage from './signInPage';
+import { createClient } from '../../utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect('/login');
+  }
   return (
     <div className={styles.main}>
       <div>
